23dd6adf720f1953a695f0bac0b6270967348c2c
flashjpr <flashjpr@gmail.com>
2023-10-05 17:50:10 +0100

    hooks

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,16 +1,15 @@
-import { createRoot } from 'react-dom';
-import Pet from './Pet';
+import { createRoot } from 'react-dom/client';
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Rhea" animal="Dog" breed="Rottweiler" />
-      <Pet name="Kiri" animal="Bird" breed="Parrot" />
-      <Pet name="Thor" animal="Dog" breed="Rottweiler" />
+      <SearchParams />
     </div>
   );
 };
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
